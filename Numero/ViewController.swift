1620090f5d3f0840726d0121ec9585d896b7c848
Naveen George Thoppan <naveen@portablemedicaltechnology.com>
2019-12-18 13:09:58 +0000

    Added comments

--- Numero/ViewController.swift
+++ Numero/ViewController.swift
@@ -26,6 +26,7 @@
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
 
+///Viewcontroller class
 import UIKit
 
 class ViewController: UIViewController {
